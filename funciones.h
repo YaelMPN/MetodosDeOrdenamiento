a5295433cdef177155fefa8a2d7b8387870bff6e
yayo <yperezn2100@alumno.ipn.mx>
2025-10-09 00:14:00 -0600

    version 1

--- funciones.h
+++ funciones.h
@@ -6,3 +6,12 @@
 #define METODOSDEORDENAMIENTO_FUNCIONES_H
 
 #endif //METODOSDEORDENAMIENTO_FUNCIONES_H
+
+
+
+#include <vector> // Necesitamos incluir vector porque lo usamos en la declaración
+
+// Declaración de la función que llenará el arreglo (vector)
+// Pasa el vector por referencia (&) para poder modificar el original
+void llenarArregloAleatorio(std::vector<int>& arr);
+
--- main.cpp
+++ main.cpp
@@ -1,11 +1,41 @@
 //
 // Created by yayo-lin on 9/10/25.
 //
+
+
+
+
+
+
+
 #include <iostream>
+#include <vector>       // Para poder usar std::vector
+#include "funciones.h"  // Incluimos el archivo con nuestras declaraciones
 
-using namespace std;
-int main () {
+int main() {
+    using namespace std;
 
-    cout << "hola";
+    cout << "Bienvenido al programa de metodos de ordenamiento\n";
+
+    int tam;
+    cout << "Dame el tamaño del arreglo\n";
+    cin >> tam;
+
+    // Creamos un vector del tamaño que el usuario indicó.
+    // Es más seguro y flexible que un arreglo C-style.
+    vector<int> arreglo(tam);
+
+    // Llamamos a la función para que llene nuestro vector con números aleatorios.
+    llenarArregloAleatorio(arreglo);
+
+    // Imprimimos el contenido del vector para verificar.
+    cout << "El contenido del arreglo generado es: \n";
+    for (int i = 0; i < arreglo.size(); ++i) {
+        cout << arreglo[i] << " ";
+    }
+    cout << endl; // Para un salto de línea al final
+
+    // cout << "Eliga "; // Aquí continuarías con el resto de tu programa
+
     return 0;
 }
