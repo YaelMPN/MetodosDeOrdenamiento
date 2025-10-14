0c0a1deb585fca0408ff058ca667c0f6e735d45b
Yael Mauricio Perez Nava <yperezn2100@alumno.ipn.mx>
2025-10-14 10:44:49 -0600

    falta busqueda binaria en menu secundario

--- main.cpp
+++ main.cpp
@@ -20,9 +20,9 @@ int main() {
     int* arreglo = new int[tam];
 
     llenar(arreglo, tam);
-
+    Mostrar(arreglo, tam);
     do {
-        Mostrar(arreglo, tam);
+
 
 
 
@@ -32,6 +32,7 @@ int main() {
         cout << "2. Ordenar Burbuja" << endl;
         cout << "3. Ordenar Inserccion" << endl;
         cout << "4. Ordenar Seleccion" << endl;
+        cout << "5. Salir" << endl;
 
 
 
@@ -40,12 +41,24 @@ int main() {
         int valor;
 
         switch (opc) {
-
             case 1:
+                system("cls");
                 cout << "Ingrese el valor a buscar" << endl;
                 cin >> valor;
 
                 BuscarSecuencial(arreglo, tam, valor);
+                cout <<"elige una opcion"<<endl;
+                cout << "1. Buscar por binario" << endl;
+                cout << "2. regresar al menu" << endl;
+                cin >> opc;
+                switch (opc) {
+                    case 1:
+                        cout<<"hola"<<endl;
+                        break;
+                    case 2:
+                        break;
+                }
+
 
 
                 break;
@@ -63,8 +76,13 @@ int main() {
                 cout << "el valor del nuevo arreglo ordenado por Seleccion es" << endl;
                 OrdenarSeleccion(arreglo, tam);
                 break;
+            case 5:
+                cout << "Saliendo..." << endl;
+                break;
+            default:
+                cout << "Opcion incorrecto, Intenta de nuevo" << endl;
         }
-    }while(opc != 0);
+    }while(opc != 5);
     delete[] arreglo;
 
     return 0;
